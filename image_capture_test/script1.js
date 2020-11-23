3aeefe2ec73b53de97a72c54b0ac00a8bb209d69
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2020-11-23 17:22:53 +0300

    image capture and output

--- image_capture_test/script1.js
+++ image_capture_test/script1.js
@@ -1,19 +1,31 @@
+let video;
 let webcamStream;
  
-  function startWebcam() {
-    navigator.mediaDevices.getUserMedia({
-      video: true
-    }).then((stream) => {
-      let video = document.querySelector('#video');
-      video.srcObject = stream;
-      video.play();
+    function startWebcam() {
  
-      webcamStream = stream;
-    }).catch((error) => {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then((stream) => {
+        video = document.querySelector('#video');
+        video.srcObject = stream;
+        video.play();
+ 
+        webcamStream = stream;
+      }).catch((error) => {
         console.log('navigator.getUserMedia error: ', error);
-    });
-  }
+      });
+    }
+    function stopWebcam() {
+          webcamStream.getTracks()[0].stop(); 
+    } 
+
+  var canvas, ctx;
  
-  function stopWebcam() {
-    webcamStream.getTracks()[0].stop(); // video
-  }
+function init() {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext('2d');
+}
+ 
+function snapshot() {
+  ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+}
