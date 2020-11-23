2828a8ce65f0e34ea0b0b46f0f4ac5619e3c45d4
AnnaKobyakova <aakobyakova_1@edu.hse.ru>
2020-11-23 23:51:17 +0300

    bodypix api without layouts

--- image_capture_test/script1.js
+++ image_capture_test/script1.js
@@ -19,13 +19,36 @@ let webcamStream;
           webcamStream.getTracks()[0].stop(); 
     } 
 
-  var canvas, ctx;
+  var canvas, canvas1, ctx;
  
 function init() {
   canvas = document.getElementById("myCanvas");
+  canvas1 = document.getElementById("myCanvas1");
   ctx = canvas.getContext('2d');
 }
+
+async function loadAndPredict(img) {
+  const net = await bodyPix.load({
+    architecture: 'MobileNetV1',
+    outputStride: 16,
+    multiplier: 0.75,
+    quantBytes: 2
+});
+    const segmentation = await net.segmentPerson(img);
+    console.log(segmentation);
+
+    const coloredPartImage = bodyPix.toMask(segmentation);
+    const opacity = 0.7;
+    const flipHorizontal = false;
+    const maskBlurAmount = 0;
+    
+    bodyPix.drawMask(
+      canvas1, img, coloredPartImage, opacity, maskBlurAmount,
+      flipHorizontal);
+  }
  
 function snapshot() {
   ctx.drawImage(video, 0,0, canvas.width, canvas.height);
+  const img = document.getElementById('myCanvas');
+  loadAndPredict(img);
 }
